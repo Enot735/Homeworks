78cebe69c420c3f6c40248bef36e7fa018eaaee3
Enot735 <kotiksv19@gmail.com>
2021-02-07 04:11:22 +1000

    ClickBaseActor

---
+++ Source/Teleport/ClickBaseActor.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseActor.h"
+#include "ClickBaseActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TELEPORT_API AClickBaseActor : public ABaseActor
+{
+	GENERATED_BODY()
+
+public:
+	AClickBaseActor();
+
+	void ClickActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
---
+++ Source/Teleport/ConeClickBaseActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ClickBaseActor.h"
+#include "ConeClickBaseActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TELEPORT_API AConeClickBaseActor : public AClickBaseActor
+{
+	GENERATED_BODY()
+	
+};
